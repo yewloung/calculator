58a3d0d2de002e8b9e6004ba80425d1ed218ed96
YewLoung <yewloung@gmail.com>
2020-10-04 16:50:40 +0800

    add calculation result title head

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ x = float(input('1st numbers : '))
 y = float(input('2nd numbers : '))
 
 print('\n')
+print('Calculation result:')
 print(x, '+', y, '=', add(x, y))
 print(x, '-', y, '=', minus(x, y))
 print(x, '*', y, '=', multiply(x, y))
