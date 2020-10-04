212f30c5c18c7073de8dc127b75d221685313834
YewLoung <yewloung@gmail.com>
2020-10-04 16:28:47 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from addition import *
+
+print('---------- This is calculator program ----------')
+print('\n')
+
+print('Please input 2 numbers')
+x = input('1st numbers : ')
+y = input('2nd numbers : ')
+
+print('\n')
+print('Addition result: ', add(x, y))
