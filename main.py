127c989991a5429d4f2f5a33c23531e787b4aae5
YewLoung <yewloung@gmail.com>
2020-10-04 16:42:36 +0800

    add multiplication feature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from addition import *
 from substraction import *
+from multiplication import *
 
 print('---------- This is calculator program ----------')
 print('\n')
@@ -11,3 +12,4 @@ y = float(input('2nd numbers : '))
 print('\n')
 print('Addition result: ', add(x, y))
 print('Substraction result: ', minus(x, y))
+print('Multiplication result: ', multiply(x, y))
